6abf2932faf4c71bfbac93d04a8c2bf8715ab35f
Juan Iriart <jeiriart@gmail.com>
2022-09-02 16:11:41 -0300

    feat: autoplay carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('.cast-carousel').slick({
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 5000,
     });
 })
