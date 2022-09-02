eacd1f928488aeea3b6ac9e12675208a65f2b3f3
Juan Iriart <jeiriart@gmail.com>
2022-09-02 16:12:33 -0300

    feat: responsiv carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,27 @@ $(document).ready(function(){
         slidesToScroll: 4,
         autoplay: true,
         autoplaySpeed: 5000,
+        fade: true,
+        cssEase: 'linear',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]
     });
 })
