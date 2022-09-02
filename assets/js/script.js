f29291fb0d084c3067cc554b4e0e5f04c5be08b9
Juan Iriart <jeiriart@gmail.com>
2022-09-02 16:33:12 -0300

    feat: shop.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,31 @@
-$(document).ready(function(){
-    $('.cast-carousel').slick({
-        centerPadding: '60px',
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-              }
-            }
-          ]
-    });
-})
+$(document).ready(function () {
+  const options = {
+    centerPadding: "60px",
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+  $(".cast-carousel").slick(options);
+  $(".shop-products").slick(options);
+});
